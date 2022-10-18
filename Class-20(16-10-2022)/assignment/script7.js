40aec5155a83d2039704e976f5c101c364cbf6b1
hmjahid <mdjahidhasan919@gmail.com>
2022-10-18 21:14:03 +0600

    Updated and added new JavaScript assignments

---
+++ Class-20(16-10-2022)/assignment/script7.js
@@ -0,0 +1,4 @@
+// querySelector Function using class
+let x = document.querySelector(".demo").style.backgroundColor = "violet";
+
+console.log(x);
---
+++ Class-20(16-10-2022)/assignment/script8.js
@@ -0,0 +1,4 @@
+// querySelectorAll Function using class
+let x = document.querySelectorAll(".demo").style.backgroundColor = "violet";
+
+console.log(x);
